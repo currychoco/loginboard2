b69648f400ca69be58d6ae9bbb100122ced6ded7
currychoco <godqhrgka0000@naver.com>
2023-10-17 10:50:45 +0900

    오토로드 테스트 성공

--- test/autoload/autoload.php
+++ test/autoload/autoload.php
@@ -1,7 +1,7 @@
 <?php
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/loginboard2/conf.php';
 
     spl_autoload_register(function($className) {
 
-        include __DIR__ . '/../classes/' . $className . '.php';
-
+        include ROOT_PATH . '/test/autoload/classes/' . $className . '.php';
     });
