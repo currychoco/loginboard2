381eb2aadd7896d07d29f2f27eb082b876e446e8
currychoco <godqhrgka0000@naver.com>
2023-10-18 10:52:04 +0900

    idCheck 수정

--- process/user/idCheck.php
+++ process/user/idCheck.php
@@ -1,13 +1,22 @@
 <?php
+    use dao\User;
+
     require_once $_SERVER['DOCUMENT_ROOT'] . '/loginboard2/conf.php';
     require_once ROOT_PATH . '/common/autoload.php';
 
     $utility = new Utility();
+    $dao = new User();
 
     $id = $utility->filter_SQL( $_GET['userId']);
     
     $result = $dao->idCheck($id);
     
-    $data = array('count'=>$result);
+    $data = null;
+    if($result) {
+        $data = array('count' => $result);
+    }
+    else {
+        $data = array('count' => '-1');
+    }
 
     echo json_encode($data);
